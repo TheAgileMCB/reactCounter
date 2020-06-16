ef2f6337993162d51df74c14ca05f81229c20b84
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-15 22:49:18 -0500

    updated header nav links

--- react-counter/src/components/Header.js
+++ react-counter/src/components/Header.js
@@ -6,10 +6,10 @@ const Header = () => (
       <h1>Welcome to this React click counter!</h1>
       <nav>
         <ul>
-          <li>We'll link you somewhere, we promise</li>
-          <li>But not here-- that would be weird</li>
-          <li>Try here. You might learn something</li>
-          <li>Or here? I don't know</li>
+          <li><a href="https://en.wikipedia.org/wiki/Goat">We'll link you somewhere, we promise</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Baphomet">But not here-- that would be weird</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Orchidaceae">Try here. You might learn something</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Fern">Or here? I don't know</a></li>
         </ul>
       </nav>
     </header>
