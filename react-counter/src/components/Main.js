fbe69c9c23191a4c207f931eff810627b2637b35
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-15 23:23:52 -0500

    working towards changing colors and buttons that increase and decrease

--- react-counter/src/components/Main.js
+++ react-counter/src/components/Main.js
@@ -4,27 +4,74 @@ class Main extends React.Component {
     constructor(props) {
       super(props);
   
+      this.textInput = React.createRef();
       this.state = {
-        clicks: 0,
+        count: 0,
+        value: 0,
+        colorClass: "zero",
+        name: "",
       };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleChange = this.handleSubmit.bind(this);
+
     }
   
     // event handler is a class prop
-    handleWord = e => {
-      let words = e.target.value;
-      this.setState({ words });
-      console.log('__STATE__', this.state);
-    }
-  
-  handleClick = e => {
+    IncreaseCount = e => {
+      e.preventDefault();
+      var previousCount = this.state.count;
+      var count = this.state.count + 1;
+    this.colorChange(previousCount, count);
+    this.setState({ count });
+  }
+
+  DecreaseCount = e => {
     e.preventDefault();
-    let words = this.state.words.split(' ').reverse().join(' ');
-  
-    this.setState({ words });
+    var previousCount = this.state.count;
+    var count = this.state.count - 1;
+  this.colorChange(previousCount, count);
+  this.setState({ count });
+  }
+
+  handleChange(event) {
+    var value = event.target.value;
+    this.setState({ value });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    var previousCount = this.state.count;
+    var count = this.state.value;
+    this.colorChange(previousCount, count);
+
+    this.setState({ count });
+
+    var value = 0;
+    this.setState({ value });
+
+    this.setState({ name: this.textInput.current.value });
+  }
+
+  colorChange = (previousCount, count) => {
+    var colorClass;
+
+    if (count < 0){
+      colorClass = "negative";
+      this.setState({ colorClass});
+    }
+
+    if (count === 0){
+      colorClass = "zero";
+      this.setState({ colorClass});
+    }
+
+    if (count > 0){
+      colorClass = "positive";
+      this.setState({ colorClass});
+    }
   }
   
     render() {
-      console.log('Rendering!', this.state)
       return (
         <div className="wrapper">
           <h2>Counting, baby!</h2>
